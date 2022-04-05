63de278c3eda6e0749528503c5f3fd5c5ba0d17f
hendri sapvilten <87599745+svn29@users.noreply.github.com>
2022-04-05 16:56:14 +0700

    UTS RosaliaN HendriS RoutePasienPoliDokter
    Membuat Route

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,22 @@ Route::get('/rekam_medis/cari','App\Http\Controllers\RekamMedisController@cari')
 Route::get('/rekam_medis/laporan','App\Http\Controllers\RekamMedisController@laporan');
 Route::get('/rekam_medis/antrian','App\Http\Controllers\RekamMedisController@antrian');
 Route::get('/rekam_medis/poli','App\Http\Controllers\RekamMedisController@poli');
+
+Route::get('/dokter','App\Http\Controllers\DokterController@dokterindex');
+
+Route::get('/pasien','App\Http\Controllers\PasienController@pasienindex');
+Route::get('/tambahpasien','App\Http\Controllers\PasienController@tambahpasien');
+Route::post('/createpasien','App\Http\Controllers\PasienController@createpasien');
+Route::get('/hapuspasien/{id}','App\Http\Controllers\PasienController@hapuspasien');
+Route::get('/ubahpasien/{id}','App\Http\Controllers\PasienController@ubahpasien');
+Route::post('/editpasien/{id}','App\Http\Controllers\PasienController@editpasien');
+
+Route::get('/poli_dash','App\Http\Controllers\PoliController@poli_dash')->name('poli_dash');
+Route::post('/poli_tambah','App\Http\Controllers\PoliController@poli_tambah')->name('poli_tambah');
+Route::get('/poli_edit/{id}','App\Http\Controllers\PoliController@poli_edit')->name('poli_edit');
+Route::post('/poli_update/{id}', '\App\Http\Controllers\PoliController@poli_update')->name('poli_update');
+Route::get('/poli_delete/{id}', '\App\Http\Controllers\PoliController@poli_delete')->name('poli_delete');
+
+Route::get('/dokter_dash','App\Http\Controllers\DokterController@dokter_dash')->name('dokter_dash');
+Route::get('/dokter_tambah','App\Http\Controllers\DokterController@dokter_tambah')->name('dokter_tambah');
+Route::get('/dokter_edit','App\Http\Controllers\DokterController@dokter_edit')->name('dokter_edit');
