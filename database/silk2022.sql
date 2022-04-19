a9cdab30fd3cd54bc963f54492decdf81a71b235
Argo Wibowo <argo.wibowo1989@gmail.com>
2022-04-19 15:56:13 +0700

    integrasi database

--- database/silk2022.sql
+++ database/silk2022.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 22, 2022 at 09:38 AM
+-- Generation Time: Apr 19, 2022 at 08:55 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -21,6 +21,257 @@ SET time_zone = "+00:00";
 --
 -- Database: `silk2022`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian_obat`
+--
+
+CREATE TABLE `antrian_obat` (
+  `id_antrian_obat` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_poli` int(11) NOT NULL,
+  `id_rm` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian_pendaftaran`
+--
+
+CREATE TABLE `antrian_pendaftaran` (
+  `id_antrian_pendaftaran` int(11) NOT NULL DEFAULT '0',
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian_poli`
+--
+
+CREATE TABLE `antrian_poli` (
+  `id_antrian_poli` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_poli` int(11) NOT NULL,
+  `no_rm` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_dokter` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama_dokter`, `created_at`, `updated_at`) VALUES
+(1, 'dr. Widana Pratama', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_04_05_085337_create_pasien_table', 1),
+(6, '2022_04_05_090121_create_poli_table', 1),
+(7, '2022_04_05_090137_create_dokter_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(5) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `kode_obat` char(20) NOT NULL,
+  `jenis_obat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama`, `kode_obat`, `jenis_obat`) VALUES
+(101, 'Analgesik', '001', 'obat cair'),
+(102, 'Antidiare', '002', 'obat tablet'),
+(103, 'Antibiotik', '003', 'obat tablet'),
+(104, 'Sitotoksi', '004', 'obat cair'),
+(105, 'Antiinflamasi', '005', 'obat cair'),
+(106, 'Bronkodilator', '006', 'obat cair'),
+(107, 'AntiCemas', '007', 'obat tablet'),
+(108, 'Antiaritmia', '008', 'obat tablet'),
+(109, 'Antijamur', '009', 'obat tablet'),
+(110, 'Dekongestan', '010', 'obat cair');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `no_rm` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ibu_kandung` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gol_darah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`no_rm`, `nama`, `alamat`, `nik`, `telp`, `ibu_kandung`, `gol_darah`, `tgl_lahir`, `jk`, `created_at`, `updated_at`) VALUES
+(1112, 'Rena', 'Jogja', '0987654341', '0987835626', 'Leni', 'B', '2022-04-04', 'Perempuan', NULL, NULL),
+(1113, 'Agus', 'Bantul', '8398475937459', '0875747474747', 'Ibunya Agus', 'O', '2022-04-19', 'Pria', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poli`
+--
+
+CREATE TABLE `poli` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_poli` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `poli`
+--
+
+INSERT INTO `poli` (`id`, `nama_poli`, `created_at`, `updated_at`) VALUES
+(1, 'Ibu dan Anak', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rekam_medis`
+--
+
+CREATE TABLE `rekam_medis` (
+  `id` bigint(20) NOT NULL,
+  `NoRM` int(11) NOT NULL,
+  `tindakan` varchar(255) NOT NULL,
+  `id_poli` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_obat`
+--
+
+CREATE TABLE `stok_obat` (
+  `id` int(5) NOT NULL,
+  `kode_obat` char(20) NOT NULL,
+  `stok` char(5) NOT NULL,
+  `tanggal_masuk` char(10) NOT NULL,
+  `exp_date` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stok_obat`
+--
+
+INSERT INTO `stok_obat` (`id`, `kode_obat`, `stok`, `tanggal_masuk`, `exp_date`) VALUES
+(1, '001', '20', '20-9-2022', '20-9-2023'),
+(2, '002', '20', '19-10-2022', '19-10-2023'),
+(3, '003', '21', '20-5-2022', '20-10-2023'),
+(4, '004', '25', '12-4-2022', '12-4-2023'),
+(5, '005', '20', '3-5-2022', '3-5-2023'),
+(6, '006', '21', '12-5-2022', '12-5-2023'),
+(7, '007', '30', '20-12-2022', '20-12-2023'),
+(8, '008', '20', '1-10-2022', '1-10-2023'),
+(9, '009', '22', '14-11-2022', '14-11-2023'),
+(10, '010', '21', '19-10-2022', '19-10-2023');
 
 -- --------------------------------------------------------
 
@@ -51,6 +302,87 @@ INSERT INTO `users` (`id`, `username`, `password`, `is_dokter`, `is_farmasi`, `i
 --
 
 --
+-- Indexes for table `antrian_obat`
+--
+ALTER TABLE `antrian_obat`
+  ADD PRIMARY KEY (`id_antrian_obat`);
+
+--
+-- Indexes for table `antrian_pendaftaran`
+--
+ALTER TABLE `antrian_pendaftaran`
+  ADD PRIMARY KEY (`id_antrian_pendaftaran`);
+
+--
+-- Indexes for table `antrian_poli`
+--
+ALTER TABLE `antrian_poli`
+  ADD PRIMARY KEY (`id_antrian_poli`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`no_rm`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `poli`
+--
+ALTER TABLE `poli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stok_obat`
+--
+ALTER TABLE `stok_obat`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -59,6 +391,60 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `no_rm` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1114;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `poli`
+--
+ALTER TABLE `poli`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `rekam_medis`
+--
+ALTER TABLE `rekam_medis`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stok_obat`
+--
+ALTER TABLE `stok_obat`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
