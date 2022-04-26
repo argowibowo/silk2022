cb2752af9ca497e4ee14ac6bdaeb1863ba15cf73
onesimusedison <onesimusedison41@gmail.com>
2022-04-26 12:14:52 +0700

    tampilan tambah

--- resources/views/view_rm/tambah.blade.php
+++ resources/views/view_rm/tambah.blade.php
@@ -2,107 +2,81 @@
 @extends('umum_template')
 
 @section('content')
-<div class="content-header row">
-
-  <div class="content-header-right col-md-12">
-    <a href="?page=rekam-medis" class="btn btn-light float-right mb-2">Kembali</a>
-  </div>
-</div>
-<div class="container-fluid">
-<section id="basic-horizontal-layouts">
-  <div class="row match-height">
-    <div class="col-md-12 col-12">
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title">Tambah Rekam Medis</h4>
-        </div>
-        <div class="card-content">
-          <div class="card-body">
-            <form action="app/rekam-medis/proses/create.php" method="post">
-              <div class="form-body">
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group row">
-                      <div class="col-md-4">
-                        <label>Pasien</label>
-                      </div>
-                      <div class="col-md-8">
-                        <select name="cars" id="cars">
-                          <option value="volvo">Edi</option>
-                          <option value="saab">Riki</option>
-                          <option value="mercedes">Kurni</option>
-                          <option value="audi">Aldi</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-12">
-                    <div class="form-group row">
-                      <div class="col-md-4">
-                        <label>Tindakan</label>
-                      </div>
-                      <div class="col-md-8">
-                        <textarea class="form-control" id="basicTextarea" rows="3" placeholder="keluhan" name="keluhan" required></textarea>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-12">
-                    <div class="form-group row">
-                      <div class="col-md-4">
-                        <label>Poli</label>
-                      </div>
-                      <div class="col-md-8">
-                        <select name="cars" id="cars">
-                          <option value="volvo">Poli 1</option>
-                          <option value="saab">Poli 2</option>
-                          <option value="mercedes">Poli 3</option>
-                          <option value="audi">Poli 4</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-12">
-                    <div class="form-group row">
-                      <div class="col-md-4">
-                        <label>Dokter</label>
-                      </div>
-                      <div class="col-md-8">
-                          <select name="cars" id="cars">
-                            <option value="volvo">Riko </option>
-                            <option value="saab">Darwin</option>
-                            <option value="mercedes">Thomas</option>
-                          </select>                    
-                      </div>
-                    </div>
-                  </div>
-
-              
-
-                  <div class="col-12">
-                    <div class="form-group row">
-                      <div class="col-md-4">
-                        <label>Tanggal Periksa</label>
-                      </div>
-                      <div class="col-md-8">
-                        <input type="text" name="tanggal" value="<?= date('Y-m-d'); ?>" readonly class="form-control">
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Save</button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+<div class="row">
+    <!-- left column -->
+    <div class="col-md-12">
+      <!-- general form elements -->
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Data Pasien Baru</h3>
+           
       </div>
+      <div class="card card-primary card-outline">
+            <div class="container-fluid">
+      <form action="{{url('createpasien')}}" method="post">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                          <div class="mb-3 mt-3 row">
+                              <label class="col-sm-2 col-form-label">Pasien</label>
+                          <div class="col-sm-10">
+                              <select name="gol_darah" class="form-control select2bs4" style="width: 30%;">
+                                  <option selected="selected">Plih Pasien</option>
+                                  <option>A</option>
+                                  <option>B</option>
+                                  <option>O</option>
+                                  <option>AB</option>
+                              </select>
+                          </div></div></div>
+                        <div class="form-group">
+                            <div class="mb-3 row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Tindakan</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputPassword" name="nik" style=width:500px>
+                        </div></div></div>
+                        <div class="form-group">
+                          <div class="mb-3 mt-3 row">
+                              <label class="col-sm-2 col-form-label">Dokter</label>
+                          <div class="col-sm-10">
+                              <select name="gol_darah" class="form-control select2bs4" style="width: 30%;">
+                                  <option selected="selected">Pilih Dokter</option>
+                                  <option>A</option>
+                                  <option>B</option>
+                                  <option>O</option>
+                                  <option>AB</option>
+                              </select>
+                          </div></div></div>
+                        <div class="form-group">
+                          <div class="mb-3 mt-3 row">
+                              <label class="col-sm-2 col-form-label">Poli</label>
+                          <div class="col-sm-10">
+                              <select name="gol_darah" class="form-control select2bs4" style="width: 30%;">
+                                  <option selected="selected">Pilih Poli</option>
+                                  <option>A</option>
+                                  <option>B</option>
+                                  <option>O</option>
+                                  <option>AB</option>
+                              </select>
+                          </div></div></div>
+
+                        
+
+                        <div class="form-group">
+                            <div class="mb-3 row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                            <div class="col-sm-10">
+                            <input type="date" class="form-control" id="inputPassword" name="tgl_lahir" style=width:160px>
+
+                        </div></div></div>
+                        
+
+                        
+                <div class="card-footer" style="float-right">
+                    <a class="btn btn-danger" href="{{url('pasienindex')}}" role="button">Batal</a>
+                    <button type="submit" class="btn btn-primary float-right" >Simpan</button>
+                </div>
+            </form>
     </div>
-  </div>
 </div>
 
 @endsection
