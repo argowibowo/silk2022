7c033b0e507c8b4e7d30135994feaf528f24e125
Argo Wibowo <argo.wibowo1989@gmail.com>
2022-05-10 14:58:25 +0700

    merapikan sidebar data master

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -17,11 +17,23 @@
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu" data-widget="tree">
       <!-- Optionally, you can add icons to the links -->
-      <li><a href="{{ url('pasienindex') }}"><i class="fa fa-users"></i> <span>Tambah Data Pasien</span></a></li>
-      <li><a href="{{ url('dokterindex') }}"><i class="fa fa-users"></i> <span>Tambah Data Dokter</span></a></li>
-      <li><a href="{{ url('obatindex') }}"><i class="fa fa-users"></i> <span>Tambah Data Obat</span></a></li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-folder"></i> <span>Master Data</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu" {{Request::is('adm/*')? 'style=display:block' : ''}}>
+          <li><a href="{{ url('pasienindex') }}"><i class="fa fa-users"></i> <span>Data Pasien</span></a></li>
+          <li><a href="{{ url('dokterindex') }}"><i class="fa fa-users"></i> <span>Data Dokter</span></a></li>
+          <li><a href="{{ url('obatindex') }}"><i class="fa fa-users"></i> <span>Data Obat</span></a></li>
+          <li><a href="{{ url('poli_dash') }}"><i class="fa fa-hospital-o"></i> <span>Data Poli</span></a></li>
+        </ul>
+      </li>
+      
       <li><a href="{{ url('transaksiobatindex') }}"><i class="fa fa-users"></i> <span>Transaksi Obat</span></a></li>
-      <li><a href="{{ url('poli_dash') }}"><i class="fa fa-hospital-o"></i> <span>Tambah Data Poli</span></a></li>
+      
       <li><a href="{{ url('rekam_medis') }}"><i class="fa fa-file-text" ></i> <span>Rekam Medis</span></a></li>
       <li><a href="{{ url('dashrawatjalan') }}"><i class="fa fa-heartbeat" ></i> <span>Rawat Jalan</span></a></li>
     </ul>
