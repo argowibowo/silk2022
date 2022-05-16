cdb3ec2ac51492c8d0a8f0b7b58a54debb17adbf
robertusang <99716000+robertusang@users.noreply.github.com>
2022-05-16 22:23:55 +0700

    RobertusLisaSprint3

---
+++ resources/views/view_antrian_pendaftaran/tambahAntrianPendaftaran.blade.php
@@ -0,0 +1,33 @@
+@extends('umum_template')
+
+@section('content')
+
+<div class="row">
+    <!-- left column -->
+    <div class="col-md-12">
+      <!-- general form elements -->
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Tambah Antrian Pendaftaran</h3>
+           
+      </div>
+      <div class="card card-primary card-outline">
+            <div class="card-body">
+      <form action="{{url('createAntrianPendaftaran')}}" method="post">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <div class="mb-3 row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal</label>
+                            <div class="col-sm-10">
+                            <input type="date" class="form-control" id="inputPassword" name="tanggal" style=width:160px>
+
+                        </div></div></div>  
+                        
+                <div class="card-footer" style="float-right">
+                    <a class="btn btn-danger" href="{{url('antrianPendaftaranIndex')}}" role="button">Batal</a>
+                    <button type="submit" class="btn btn-primary float-right" >Simpan</button>
+                </div>
+            </form>
+    </div>
+</div>
+@endsection
