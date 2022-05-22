12643205a63b6a47b3ef287c57b4498e0dc4deba
hendri sapvilten <87599745+svn29@users.noreply.github.com>
2022-05-22 23:17:29 +0700

    Update Checkup Tambah Hendri Rosa Sprint 4

--- resources/views/checkup_tambah.blade.php
+++ resources/views/checkup_tambah.blade.php
@@ -13,29 +13,32 @@
       </div>
       <div class="card card-primary card-outline">
             <div class="card-body">
-      <form action="{{url('createcheckup')}}" method="post">
+            <form action="{{url('createcheckup')}}" method="post">
                         {{ csrf_field() }}
                             <div class="form-group">
                             <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">No Rekam Medis</label>
+                            <label for="no_rm" class="col-sm-2 col-form-label">No Rekam Medis</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="nama" style=width:500px>
+                            <input type="text" class="form-control" id="no_rm" name="no_rm" style=width:500px>
                         </div></div></div>
-                        <div class="form-group">
-                            <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Nama</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="alamat" style=width:500px>
-                        </div></div></div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal</label>
+                                <div class="col-sm-10">
+                                    <input type="date" id="inputPassword" name="tanggal" style=width:180px class="form-control
+                                    @error('tanggal') is-invalid @enderror" value="{{old('tanggal')}}">
+                                    @error('tanggal')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                        </div>
                         <div class="form-group">
                         <div class="mb-3 mt-3 row">
                             <label class="col-sm-2 col-form-label">Poli</label>
                         <div class="col-sm-10">
-                            <select name="gol_darah" class="form-control select2bs4" style="width: 30%;">
-                                <option selected="selected">Pilih Poli</option>
-                                <option>Poli Ibu dan Anak</option>
-                                <option>Poli Kandungan</option>
-                                <option>Poli Jantung</option>
+                            <select name="id_poli" class="form-control select2bs4" style="width: 30%;">
+                            @foreach($poli as $p)   
+                            <option value="{{$p->id}}">{{$p->nama_poli}}</option>
+                            @endforeach
                             </select>
                         </div></div></div>
                 <div class="card-footer" style="float-right">
@@ -43,6 +46,29 @@
                     <button type="submit" class="btn btn-primary float-right" >Simpan</button>
                 </div>
             </form>
+            <!-- <script language="JavaScript" type="text/javascript" src="/js/jquery-1.2.6.min.js"></script>
+            <script language="JavaScript" type="text/javascript" src="/js/jquery-ui-personalized-1.5.2.packed.js"></script>
+            <script language="JavaScript" type="text/javascript" src="/js/sprinkle.js"></script>
+           <script type="text/javascript"> $("#no_rm").focusout(function(e){
+                    // alert($(this).val());
+                    var no_rm = $(this).val();
+                    // var niuser1 = $(this).val();
+                    $.ajax({
+                        type: "GET",
+                        url: "{{route('autocomplete')}}",
+                        data: {'no_rm':no_rm},
+                        dataType: 'json',
+                        success : function(data) {
+                            $('#name').val(data.name);
+                            
+                        },
+                        error: function(response) {
+                            alert(response.responseJSON.message);
+                        }
+                    });
+
+                });
+                </script> -->
     </div>
 </div>
 @endsection
