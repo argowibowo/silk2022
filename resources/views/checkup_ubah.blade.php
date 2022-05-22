f994fa1d1df369cb7c517808438a9efd5c781927
hendri sapvilten <87599745+svn29@users.noreply.github.com>
2022-05-22 23:15:52 +0700

    update checkup ubah Hendri Rosa Sprint 4

--- resources/views/checkup_ubah.blade.php
+++ resources/views/checkup_ubah.blade.php
@@ -13,31 +13,22 @@
       </div>
       <div class="card card-primary card-outline">
             <div class="card-body">
-            @foreach($pasien as $p)
-      <form action="{{ url('checkup_edit',$p->no_rm) }}" method="post">
+      <form action="{{ url('checkup_update',$antrian_poli->id) }}" method="post">
                         {{ csrf_field() }}
-
                             <div class="form-group">
                             <div class="mb-3 row">
                             <label for="inputPassword" class="col-sm-2 col-form-label">No Rekam Medis</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="no_rm" value="{{ $p->no_rm}}" style=width:500px>
-                        </div></div></div>
-                        <div class="form-group">
-                            <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Nama</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="nama" value="{{ $p->nama}}" style=width:500px>
+                            <input type="text" class="form-control" id="no_rm" name="no_rm" value="{{ $antrian_poli->no_rm}}" style=width:500px>
                         </div></div></div>
                         <div class="form-group">
                         <div class="mb-3 mt-3 row">
                             <label class="col-sm-2 col-form-label">Poli</label>
                         <div class="col-sm-10">
-                            <select name="gol_darah" class="form-control select2bs4"  style="width: 30%;">
-                                <option selected="selected">{{ $p->gol_darah }}</option>
-                                <option>Poli Ibu dan Anak</option>
-                                <option>Poli Kandungan</option>
-                                <option>Poli Jantung</option>
+                            <select name="id_poli" class="form-control select2bs4"  style="width: 30%;">
+                            @foreach($poli as $p)    
+                            <option value="{{$p->id}}">{{$p->nama_poli}}</option>
+                            @endforeach
                             </select>
                         </div></div></div>
                        
@@ -46,7 +37,6 @@
                     <button type="submit" class="btn btn-primary float-right" >Simpan</button>
                 </div>
             </form>
-            @endforeach
     </div>
 </div>
 @endsection
