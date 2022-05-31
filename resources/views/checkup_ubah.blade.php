49545ea2fb98185c3e6b5bc0b89c6348b13238f2
hendri sapvilten <87599745+svn29@users.noreply.github.com>
2022-05-31 14:25:57 +0700

    Update views checkup_ubah Hendri Rosa

--- resources/views/checkup_ubah.blade.php
+++ resources/views/checkup_ubah.blade.php
@@ -13,27 +13,39 @@
       </div>
       <div class="card card-primary card-outline">
             <div class="card-body">
-      <form action="{{ url('checkup_update',$antrian_poli->id) }}" method="post">
+            @foreach($antrian_poli as $a)     
+      <form action="{{ url('checkup_update',$a->id) }}" method="post">
+      @endforeach
                         {{ csrf_field() }}
-                            <div class="form-group">
+                        @foreach($antrian_poli as $a)
+                        <div class="form-group">
                             <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">No Rekam Medis</label>
+                            <label for="inputPassword" class="col-sm-2 col-form-label">No RM</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="no_rm" name="no_rm" value="{{ $antrian_poli->no_rm}}" style=width:500px>
+                            <input type="text" class="form-control" id="inputPassword" name="no_rm" value="{{ $a->no_rm}}" style=width:500px disabled>
                         </div></div></div>
+                        <div class="form-group">
+                            <div class="mb-3 row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputPassword" name="nama" value="{{ $a->nama}}" style=width:500px disabled>
+                        </div></div></div>
+                        @endforeach
                         <div class="form-group">
                         <div class="mb-3 mt-3 row">
                             <label class="col-sm-2 col-form-label">Poli</label>
                         <div class="col-sm-10">
                             <select name="id_poli" class="form-control select2bs4"  style="width: 30%;">
-                            @foreach($poli as $p)    
+                            @foreach($antrian_poli as $an)
+                            <option value="{{$an->id}}" selected="selected">{{$an->nama_poli}}</option>
+                            @endforeach
+                            @foreach($poli as $p)
                             <option value="{{$p->id}}">{{$p->nama_poli}}</option>
                             @endforeach
                             </select>
                         </div></div></div>
-                       
                 <div class="card-footer" style="float-right">
-                <a class="btn btn-danger" href="{{url('pasienindex')}}" role="button">Batal</a>
+                <a class="btn btn-danger" href="{{url('checkup')}}" role="button">Batal</a>
                     <button type="submit" class="btn btn-primary float-right" >Simpan</button>
                 </div>
             </form>
