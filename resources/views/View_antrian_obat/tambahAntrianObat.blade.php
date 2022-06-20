2ee705f6547628221b3f2a37cfb6a928ea3c476e
robertusang <99716000+robertusang@users.noreply.github.com>
2022-06-20 19:14:19 +0700

    Update tambahAntrianObat.blade.php

--- resources/views/View_antrian_obat/tambahAntrianObat.blade.php
+++ resources/views/View_antrian_obat/tambahAntrianObat.blade.php
@@ -9,38 +9,46 @@
       <div class="box box-primary">
         <div class="box-header with-border">
           <h3 class="box-title">Tambah Antrian Obat</h3>
-           
+
       </div>
       <div class="card card-primary card-outline">
-            <div class="card-body">
-      <form action="{{url('createAntrianPoli')}}" method="post">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal</label>
-                            <div class="col-sm-10">
-                            <input type="date" class="form-control" id="inputPassword" name="tanggal" style=width:160px>
-                        </div></div></div>  
+        <div class="card-body">
+            <form action="{{route('createAntrianObat')}}" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal</label>
+                    <div class="col-sm-10">
+                        <input type="date" class="form-control" id="inputPassword" name="tanggal" style=width:160px>
+                    </div></div></div>  
 
-                        <div class="form-group">
-                            <div class="mb-3 row">
+                    <div class="form-group">
+                        <div class="mb-3 row">
                             <label for="inputPassword" class="col-sm-2 col-form-label">ID Poli</label>
                             <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="id_poli" style=width:500px>
-                        </div></div></div>
+                                <select class="form-control" style=width:500px name="id_poli">
+                                    @foreach($poli as $pl)
+                                    <option value="{{$pl->id}}">{{$pl->nama_poli}}</option>
+                                    @endforeach
+                                </select>
+                            </div></div></div>
 
-                        <div class="form-group">
-                            <div class="mb-3 row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">No Rekam Medis</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" name="no_rm" style=width:500px>
-                        </div></div></div>
-                        
-                <div class="card-footer" style="float-right">
-                    <a class="btn btn-danger" href="{{url('antrianPoliIndex')}}" role="button">Batal</a>
-                    <button type="submit" class="btn btn-primary float-right" >Simpan</button>
-                </div>
-            </form>
-    </div>
-</div>
-@endsection
+                            <div class="form-group">
+                                <div class="mb-3 row">
+                                    <label for="inputPassword" class="col-sm-2 col-form-label">ID Rekam Medis</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-control" style="width: 500px;" name="id_rm">
+                                            @foreach($rekam_medis as $rm)
+                                            <option value="{{$rm->id_rm}}">{{$rm->id_rm}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div></div></div>
+
+                                    <div class="card-footer" style="float-right">
+                                        <a class="btn btn-danger" href="{{url('antrianObatIndex')}}" role="button">Batal</a>
+                                        <button type="submit" class="btn btn-primary float-right" >Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        @endsection
